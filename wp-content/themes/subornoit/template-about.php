3b0f7298df6c4af8aa0af210d9f0874f6d9c79e1
RusulAzomSumon <18103020@iubat.edu>
2022-09-28 20:09:46 +0600

    template pert activate, home and about

--- wp-content/themes/subornoit/template-about.php
+++ wp-content/themes/subornoit/template-about.php
@@ -109,58 +109,8 @@
         </div>
         <!-- Testimonial End -->
 
-        <!-- Recent Area Start -->
-        <div class="recent-area section-paddingt">
-            <div class="container">
-                <!-- section tittle -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="section-tittle text-center">
-                            <h2>Our Recent News</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xl-4 col-lg-4 col-md-6">
-                        <div class="single-recent-cap mb-30">
-                            <div class="recent-img">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/recent/rcent_1.png" alt="">
-                            </div>
-                            <div class="recent-cap">
-                                <span>Business planing</span>
-                                <h4><a href="#">Amazing Places To Visit In Summer</a></h4>
-                                <p>Nov 30, 2020</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6">
-                        <div class="single-recent-cap mb-30">
-                            <div class="recent-img">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/recent/rcent_2.png" alt="">
-                            </div>
-                            <div class="recent-cap">
-                                <span>Audit</span>
-                                <h4><a href="single-blog.html">Amazing Places To Visit In Summer</a></h4>
-                                <p>Nov 30, 2020</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6">
-                        <div class="single-recent-cap mb-30">
-                            <div class="recent-img">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/recent/rcent_3.png" alt="">
-                            </div>
-                            <div class="recent-cap">
-                                <span>Business planing</span>
-                                <h4><a href="single-blog.html">Amazing Places To Visit In Summer</a></h4>
-                                <p>Nov 30, 2020</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Recent Area End-->
+        <!-- blog showing from template part -->
+        <?php get_template_part('template-parts/pertials/content','blog'); ?>
 
     </main>
    <!-- including footer -->
---
+++ wp-content/themes/subornoit/template-parts/pertials/content-blog.php
@@ -0,0 +1,56 @@
+<!-- Recent News Area Start -->
+<div class="recent-area section-paddingt">
+            <div class="container">
+                <!-- section tittle -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section-tittle text-center">
+                            <h2>Our Recent News</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- php code for single testimonial and sql query -->
+                    <!-- dynamic testimonial  query -->
+                    <?php 
+                        $args = array(
+                            'post_type' => 'post',
+                            'order' => 'DSC',
+                            'posts_per_page' => 3
+                        );
+                        $query = new WP_Query($args);
+                        while($query -> have_posts()){
+                            $query->the_post();
+                            ?>
+                            <!-- Single News  scripts -->
+                            <div class="col-xl-4 col-lg-4 col-md-6">
+                                <div class="single-recent-cap mb-30">
+                                    <div class="recent-img">
+                                        <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_post_thumbnail_caption($post); ?>">
+                                    </div>
+                                    <div class="recent-cap">
+                                        <span>
+                                            <?php  
+                                                $catgs = get_the_category();
+                                                foreach($catgs as $catg){
+                                                    echo $catg -> name.", ";
+                                                }
+                                            ?>
+                                        </span>
+                                        <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                                        <p><?php the_date(); ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- single News script  end -->
+                        <!-- rest of loop -->
+                    <?php
+                        }
+                        wp_reset_postdata();
+                    ?>
+                    <!-- recent single news end -->
+                </div>
+            </div>
+        </div>
+        <!-- TODO:see all pagignation  -->
+        <!-- Recent Area End-->

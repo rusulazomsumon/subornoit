6323007ee844ad94b95b79496c4ba7ac32dc10bd
RusulAzomSumon <18103020@iubat.edu>
2023-03-25 23:03:31 +0600

    update: 404 page

--- wp-content/themes/subornoit/404.php
+++ wp-content/themes/subornoit/404.php
@@ -5,12 +5,20 @@
         <!-- section tittle -->
         <div class="row">
             <div class="col-lg-12">
-                <div class="section-tittle text-center">
+                <!-- <div class="section-tittle text-center">
                     <img src="https://cdn.dribbble.com/users/644529/screenshots/2662296/404.gif" 
                         alt="suborno it - 404 not found page" width="40%" height="auto">
                     <h4>Sorry! Nothig Found</h4>
                     <a href="<?php echo get_site_url(); ?>" class="btn trusted-btn">Back To Home</a>
+                </div> -->
+                <div class="container text-center">
+                    <h1>Oops! 404 Error</h1>
+                    <p>The page you're looking for doesn't exist.</p>
+                    <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="404 Error" class="img-fluid">
+                    <p>But don't worry, we'll take you back to safety.</p>
+                    <a href="/" class="btn btn-primary">Go back to home</a>
                 </div>
+
             </div>
         </div>
     </div>
